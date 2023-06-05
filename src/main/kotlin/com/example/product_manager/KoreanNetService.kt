dd64993c17371da392c0183950310585ef8348d6
llsg822 <llsg822@llsg822.local>
2023-06-05 10:42:56 +0900

    id와 pw를 webClient로 이동

--- src/main/kotlin/com/example/product_manager/KoreanNetService.kt
+++ src/main/kotlin/com/example/product_manager/KoreanNetService.kt
@@ -2,24 +2,15 @@ package com.example.product_manager
 
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToMono
-import org.springframework.web.util.UriComponentsBuilder
 
 @Component
 class KoreanNetService(
    val koreanNetWebClient: WebClient
 ) {
     fun getProduct(gtin: String) {
-        val uri = UriComponentsBuilder
-            .fromUriString("http://api.koreannet.or.kr/mobileweb/search/barcodeSearchXml/chk/${gtin}")
-            .queryParam("id", "aswemake01")
-            .queryParam("pw", "aswemake098")
-            .build()
-            .toUri()
-
         val response = koreanNetWebClient
             .get()
-            .uri(uri)
+            .uri("/mobileweb/search/barcodeSearchXml/chk/${gtin}")
             .retrieve()
             .bodyToMono(String::class.java)
             .block()
--- src/main/kotlin/com/example/product_manager/WebClientConfig.kt
+++ src/main/kotlin/com/example/product_manager/WebClientConfig.kt
@@ -2,7 +2,9 @@ package com.example.product_manager
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.util.UriComponentsBuilder
 
 @Configuration
 class WebClientConfig {
@@ -10,6 +12,19 @@ class WebClientConfig {
     fun koreanNetWebClient(): WebClient {
         return WebClient
             .builder()
+            .baseUrl("http://api.koreannet.or.kr")
+            .filter { request, next ->
+                val uri = UriComponentsBuilder
+                    .fromUri(request.url())
+                    .queryParam("id", "aswemake01")
+                    .queryParam("pw", "aswemake098")
+                    .build()
+                    .toUri()
+                val filtered = ClientRequest.from(request)
+                    .url(uri)
+                    .build()
+                next.exchange(filtered)
+            }
             .build()
     }
 }
